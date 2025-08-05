16415f5b062b12f062291c4549f29d29975d0883
adityasharma0903 <adityasharma.5672@gmail.com>
2025-08-05 15:17:50 +0530

    Added admin upload

---
+++ app/admin/upload/page.tsx
@@ -0,0 +1,10 @@
+// app/admin/upload/page.tsx
+import CSVUploader from "@/components/admin/CSVUpload"
+
+export default function UploadPage() {
+  return (
+    <main className="p-6">
+      <CSVUploader />
+    </main>
+  )
+}
